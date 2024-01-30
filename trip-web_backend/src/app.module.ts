5c524e51be7ccc6029fea2e441a8d364b159b33c
symbicort <jeongwonlee0@gmail.com>
2024-01-30 11:38:01 +0900

    userDto folder 생성

--- trip-web_backend/src/app.module.ts
+++ trip-web_backend/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
+import { UserModule } from './users/user.module';
+import { BoardsModule } from './boards/boards.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, BoardsModule],
   controllers: [],
   providers: [],
 })
