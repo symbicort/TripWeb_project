649e311720c036773a145e7f39da22859cbe912d
symbicort <jeongwonlee0@gmail.com>
2024-02-07 13:41:09 +0900

    aws service setting

--- trip-web_backend/src/aws/aws.service.ts
+++ trip-web_backend/src/aws/aws.service.ts
@@ -10,10 +10,12 @@ export class AwsService {
   constructor(private configService: ConfigService) {
     // AWS S3 클라이언트 초기화. 환경 설정 정보를 사용하여 AWS 리전, Access Key, Secret Key를 설정.
     this.s3Client = new S3Client({
-      region: this.configService.get('AWS_REGION'), // AWS Region
+      region: this.configService.get(process.env.AWS_REGION), // AWS Region
       credentials: {
-        accessKeyId: this.configService.get('AWS_S3_ACCESS_KEY'), // Access Key
-        secretAccessKey: this.configService.get('AWS_S3_SECRET_ACCESS_KEY'), // Secret Key
+        accessKeyId: this.configService.get(process.env.AWS_ACCESS_KEY_ID), // Access Key
+        secretAccessKey: this.configService.get(
+          process.env.AWS_SECRET_ACCESS_KEY,
+        ), // Secret Key
       },
     });
   }
@@ -25,7 +27,7 @@ export class AwsService {
   ) {
     // AWS S3에 이미지 업로드 명령을 생성합니다. 파일 이름, 파일 버퍼, 파일 접근 권한, 파일 타입 등을 설정합니다.
     const command = new PutObjectCommand({
-      Bucket: this.configService.get('AWS_S3_BUCKET_NAME'), // S3 버킷 이름
+      Bucket: this.configService.get(process.env.AWS_S3_BUCKET_PROFILE), // S3 버킷 이름
       Key: fileName, // 업로드될 파일의 이름
       Body: file.buffer, // 업로드할 파일
       ACL: 'public-read', // 파일 접근 권한
@@ -33,9 +35,11 @@ export class AwsService {
     });
 
     // 생성된 명령을 S3 클라이언트에 전달하여 이미지 업로드를 수행합니다.
-    await this.s3Client.send(command);
+    const imgResult = await this.s3Client.send(command);
+
+    console.log('이미지 업로드 확인', imgResult);
 
     // 업로드된 이미지의 URL을 반환합니다.
-    return `https://s3.${process.env.AWS_REGION}.amazonaws.com/${process.env.AWS_S3_BUCKET_NAME}/${fileName}`;
+    // return `https://s3.${process.env.AWS_REGION}.amazonaws.com/${process.env.AWS_S3_BUCKET_NAME}/${fileName}`;
   }
 }
