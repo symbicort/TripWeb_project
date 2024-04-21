123832633875528bd0081fa4c0f85e00454be2db
symbicort <jeongwonlee0@gmail.com>
2024-04-21 23:01:50 +0900

    유저 정보 주석 추가

--- trip-web_backend/src/users/users.controller.ts
+++ trip-web_backend/src/users/users.controller.ts
@@ -156,7 +156,7 @@ export class UsersController {
 
       if (logintoken) {
         const result: userInfoDto =
-          // 유저 정보 불러오기
+          // 유저 정보 불러오기 - /user/info
           await this.userService.getUserInfo(logintoken);
 
         res.send({
