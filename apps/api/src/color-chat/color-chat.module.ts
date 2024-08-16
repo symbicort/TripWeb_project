27c0363c8ab0de0fee809a2ce3d18c4667ba70cb
symbicort <jeongwonlee0@gmail.com>
2024-08-16 16:41:28 +0900

    fix: auth controller re init

--- apps/api/src/color-chat/color-chat.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ColorChatController } from './color-chat.controller';
-import { ColorChatService } from './color-chat.service';
-
-@Module({
-  controllers: [ColorChatController],
-  providers: [ColorChatService],
-})
-export class ColorChatModule {}
--- apps/api/src/users/users.model.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-  user_id: string;
-  password: string;
-  email: string;
-  nickname: string;
-  created_at: Date;
-  profile_img: undefined | string;
-}
