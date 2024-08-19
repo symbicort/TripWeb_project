eebb9887db5dd67c919982a880b1db5890f50174
symbicort <jeongwonlee0@gmail.com>
2024-08-19 22:56:53 +0900

    auth controller

--- apps/server/src/auth/auth.controller.ts
+++ apps/server/src/auth/auth.controller.ts
@@ -1,8 +1,11 @@
-import { Controller, Get, Header, Query, Res } from '@nestjs/common';
-import { Response } from 'express';
+import { Controller, Get, Header, Req, Res } from '@nestjs/common';
+import { Response, Request } from 'express';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
   @Get('kakao-login-page')
   @Header('Content-Type', 'text/html')
   async kakaoRedirect(@Res() res: Response): Promise<void> {
@@ -11,7 +14,8 @@ export class AuthController {
   }
 
   @Get('kakao')
-  async getKakaoInfo(@Query() code: string) {
-    console.log(code);
+  async getKakaoInfo(@Req() req: Request) {
+    const code = String(req.query.code);
+    await this.authService.kakaoLogin(code);
   }
 }
--- apps/server/src/auth/auth.service.ts
+++ apps/server/src/auth/auth.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async kakaoLogin(code: string) {
+    const REST_API_KEY = String(process.env.KAKAO_REST_API_KEY);
+    const REDIRECT_URI = String(process.env.KAKAO_REDIRECT_URI);
+    const AUTHORIZE_CODE = code;
+
+    const url = 'https://kauth.kakao.com/oauth/token';
+
+    const params = new URLSearchParams();
+    params.append('grant_type', 'authorization_code');
+    params.append('client_id', REST_API_KEY);
+    params.append('redirect_uri', REDIRECT_URI);
+    params.append('code', AUTHORIZE_CODE);
+
+    try {
+      const response = await axios.post(url, params.toString(), {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      });
+
+      console.log('Access Token:', response.data);
+      return response.data;
+    } catch (error) {
+      console.error(
+        'Error fetching token:',
+        error.response ? error.response.data : error.message,
+      );
+    }
+  }
+}
