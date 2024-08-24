c2b4797e3d10f74f63c2a0091fa7953a341c3f20
symbicort <jeongwonlee0@gmail.com>
2024-08-24 23:13:27 +0900

    feat: jwt sign 메소드 추가

--- apps/server/src/auth/auth.service.ts
+++ apps/server/src/auth/auth.service.ts
@@ -2,39 +2,41 @@ import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import axios from 'axios';
-import { profile } from 'console';
 import { UsersEntity } from 'src/users/entities/users-entity';
 import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthService {
-    constructor(@InjectRepository(UsersEntity) private usersDB: Repository<UsersEntity>,private readonly jwtService: JwtService) {}
-    
+  constructor(
+    @InjectRepository(UsersEntity) private usersDB: Repository<UsersEntity>,
+    private readonly jwtService: JwtService,
+  ) {}
 
-  async kakaoLogin(kakaoID: number, nickname: string, profile_img:string): Promise<any> {
+  async kakaoLogin(
+    kakaoID: number,
+    nickname: string,
+    profile_img: string,
+  ): Promise<any> {
     try {
       console.log('유저 정보', kakaoID, nickname, profile_img);
 
       const existUser = await this.usersDB.count({
-        where: {kakaoID: kakaoID}
-      })
+        where: { kakaoID: kakaoID },
+      });
 
-      if(existUser === 0){
+      if (existUser === 0) {
         const userData = {
-            kakaoID,
-            nickname,
-            profile_img,
-        }
+          kakaoID,
+          nickname,
+          profile_img,
+        };
 
-        const result = await this.usersDB.insert(userData)
-
-        console.log(result)
-        // return result
+        const result = await this.usersDB.insert(userData);
       }
 
-      
+      const payload = 
 
-
+      const accessToken = this.jwtService.sign()
 
     } catch (error) {
       console.error(
